393d44766d228123d802c62dd5ef74b1d739ee15
seungjae <juaje03@naver.com>
2024-03-21 18:33:56 +0900

    Add: getInstance Method

--- shop/src/controller/ItemManager.java
+++ shop/src/controller/ItemManager.java
@@ -1,5 +1,19 @@
 package controller;
 
-public class ItemManager {
+import java.util.ArrayList;
 
+import dto.Item;
+
+public class ItemManager {
+	private ArrayList<Item> itemList;
+
+	private ItemManager() {
+		itemList = new ArrayList<>();
+	}
+
+	private static ItemManager instance = new ItemManager();
+
+	public static ItemManager getInstance() {
+		return instance;
+	}
 }
