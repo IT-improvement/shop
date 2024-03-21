ada6d3cd139c7fd452a830bd2f0a9c1b4a09e6a2
seungjae <juaje03@naver.com>
2024-03-21 16:33:39 +0900

    Add : getter, setter

--- shop/src/dto/User.java
+++ shop/src/dto/User.java
@@ -1,5 +1,41 @@
 package dto;
 
 public class User {
+	private String name;
+	private String id;
+	private String password;
+	private String money;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getMoney() {
+		return money;
+	}
+
+	public void setMoney(String money) {
+		this.money = money;
+	}
 
 }
