e2c5e12b664cebab66c1e9565b27ec7483a3c5b2
seungjae <juaje03@naver.com>
2024-03-22 11:30:45 +0900

    Add: variable(count)

--- shop/src/dto/Cart.java
+++ shop/src/dto/Cart.java
@@ -3,9 +3,28 @@ package dto;
 import java.util.ArrayList;
 
 public class Cart {
-	ArrayList<Item> carts;
+	private ArrayList<Item> carts;
+	private ArrayList<Integer> count;
 
 	public Cart() {
 		carts = new ArrayList<>();
+		count = new ArrayList<>();
 	}
+
+	public ArrayList<Item> clone() {
+		return (ArrayList<Item>) carts.clone();
+	}
+
+	public void setCarts(ArrayList<Item> carts) {
+		this.carts = carts;
+	}
+
+	public ArrayList<Integer> getCount() {
+		return (ArrayList<Integer>) count.clone();
+	}
+
+	public void setCount(ArrayList<Integer> count) {
+		this.count = count;
+	}
+
 }
