e106e4f955ab04f2ce76dab07683e166158b9b3d
seungjae <juaje03@naver.com>
2024-03-22 09:12:48 +0900

    Add: addItem Method

--- shop/src/controller/ItemManager.java
+++ shop/src/controller/ItemManager.java
@@ -16,4 +16,26 @@ public class ItemManager {
 	public static ItemManager getInstance() {
 		return instance;
 	}
+
+	/* C(insert) */
+	public int creatCode() {
+		while (true) {
+			boolean isCheck = true;
+			int rNum = (int) (Math.random() * 9000) + 1000;
+			for (Item e : itemList)
+				if (e.getCode() == rNum)
+					isCheck = false;
+			if (isCheck)
+				return rNum;
+		}
+	}
+
+	public boolean add(Item item) {
+		itemList.add(item);
+		return true;
+	}
+	/* R(select) */
+	/* U(update) */
+	/* D(delete) */
+
 }
