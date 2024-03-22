beeef3c5d29d0ac13b13e1a1d097d207aae93db6
seungjae <juaje03@naver.com>
2024-03-22 12:32:36 +0900

    Add: variable(size) + getter, setter about size

--- shop/src/dto/Cart.java
+++ shop/src/dto/Cart.java
@@ -5,17 +5,25 @@ import java.util.ArrayList;
 public class Cart {
 	private ArrayList<Item> carts;
 	private ArrayList<Integer> count;
+	private int size;
+
 
 	public Cart() {
 		carts = new ArrayList<>();
 		count = new ArrayList<>();
+		size = carts.size();
 	}
 
+	public int getSize() {
+		return size;
+	}
+	
 	public ArrayList<Item> clone() {
 		return (ArrayList<Item>) carts.clone();
 	}
 
 	public void setCarts(ArrayList<Item> carts) {
+		size = carts.size();
 		this.carts = carts;
 	}
 
