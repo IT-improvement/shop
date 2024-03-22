83e1e900ec6a2c0ff10778c6315b9278329ad728
seungjae <juaje03@naver.com>
2024-03-22 14:59:01 +0900

    Add: constant, Constructor(private), getInstance Method

--- shop/src/controller/FileManager.java
+++ shop/src/controller/FileManager.java
@@ -1,5 +1,20 @@
 package controller;
 
+import java.io.File;
+
 public class FileManager {
 
+	final String ITEM = "item.txt";
+	final String USER = "user.txt";
+	final String LOG = "log.txt";
+
+	private FileManager() {
+
+	}
+
+	private static FileManager instance = new FileManager();
+
+	public static FileManager getInstance() {
+		return instance;
+	}
 }
